fbddc0734f05b76199db78b780d60b1bd107bcb0
Daniel Thacker <88279373+DanielThacker2000@users.noreply.github.com>
2023-10-22 11:18:11 +0100

    Update filterbank.py

--- Project 1/filterbank.py
+++ Project 1/filterbank.py
@@ -3,14 +3,10 @@
 Created on Sun Oct 15 09:40:37 2023
 
 @author: yad23rju
+
+MAIN FEATURE EXTRACTION CODE. Reads .wav file in the folder 'Recordings' and outputs mfcc's'
 """
 
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
 import numpy as np
 import matplotlib.pyplot as plt
 import sounddevice as sd
@@ -18,16 +14,12 @@ import plotly.io as pio
 import plotly.express as px
 import soundfile as sf
 from scipy.fftpack import dct
+import glob 
 
-
+#Set params
 FRAME_LENGTH = 1024 #Number of bins in each window (halved eventually). IDEALLY KEEP AS A RESULT OF 2^x
 STEP_LENGTH = FRAME_LENGTH * (1/2)
 FILTER_NUMBER = 40 #Number of triangle peaks each window is being multiplied by
-#SAMPLE_FREQUENCY = 16000 Currently let the sound file decide frequency
-
-r, fs2 = sf.read('Dave0.wav', dtype='float32')
-SAMPLE_FREQUENCY = fs2 # Let the sound file decide frequency
-r = r[50000:80000] # Take window of signal just for test purposes
 
 def mel_scale(data):
     return 2595*np.log10(1+(data/700))
@@ -36,26 +28,30 @@ def inverse_mel(data):
 
     return 700*(10**(data/2595) -1)
     
-def plot_melscale():
+def plot_melscale(sample_frequency):
     #Take sampling rate and upper and lower bounds of desired frqeuency
-    freq_high = SAMPLE_FREQUENCY/2 #Nyquist
+    freq_high = sample_frequency/2 #Nyquist
+    
     freq_low = 0 
+    
     mel_freq_high = mel_scale(freq_high)
 
     mel_freq_low = mel_scale(freq_low)
+    
     #Spread out the mel freqs between the high and low value
     mel_range = np.linspace(mel_freq_low, mel_freq_high, FILTER_NUMBER +2 )
 
+    #find frequency of these mel numbers
     freq_range = inverse_mel(mel_range)
-    
-    bins = np.floor((FRAME_LENGTH + 1) * freq_range / SAMPLE_FREQUENCY)
+        
+    #convert frequencies into bins
+    bins = np.floor((FRAME_LENGTH + 1) * freq_range / sample_frequency)
 
-    #code is a variation of: https://haythamfayek.com/2016/04/21/speech-processing-for-machine-learning.html
+    #set filterbank array to be filled
     fbank = np.zeros((FILTER_NUMBER, int(np.floor(FRAME_LENGTH / 2 ))))
-
-
+    #code is a variation of: https://haythamfayek.com/2016/04/21/speech-processing-for-machine-learning.html
     for m in range(1, FILTER_NUMBER + 1):
-        #left
+        #left bin value
         f_m_minus = int(bins[m - 1])  
         #middle
         f_m = int(bins[m])
@@ -69,53 +65,41 @@ def plot_melscale():
         for k in range(f_m, f_m_plus):
             fbank[m - 1, k] = (bins[m + 1] - k) / (bins[m + 1] - bins[m])
     
-    return fbank
-
-
-    
+    return fbank  
 
 def mag_and_phase(section):
-    array = np.array([])
+    #Calculate the fft and power
+    
     section_squeezed = section.squeeze()
-    for element in section:
-        array = np.append(array, element)
-
+    section = np.array(section)
     #Perform and apply hamming window
-    hamming = np.hamming(len(array))
-    scaled = np.multiply(hamming,array) 
+    hamming = np.hamming(len(section))
+    scaled = np.multiply(hamming,section) 
     fft_sample = np.fft.fft(scaled) 
     absolute = np.abs(fft_sample)
-    phase = np.angle(fft_sample)
     #Take 'power' of spectrum
-    power = absolute**2/len(array)
+    power = absolute**2/len(section)
 
     return power
-    
-    """    PLOT POWER SPECTRUM
-    f2,[ax4, ax5] = plt.subplots(nrows=2, ncols=1)
-    ax4.plot(absolute)
-    #ax5.plot(phase)#plot phase
-
-
-    #ax5.plot(power)#plot power
-    #ax4.set_title('Magnitude')
-    #ax5.set_title('power')
-    """
-
 
 def entire_utterance(speech):
-
+    #Window signal in steps and call the fft function above to find the mag and power
     num_samples = len(speech)
+    
     step_length = STEP_LENGTH
+    
     step_length = int(step_length)
+    
     num_frames = num_samples/FRAME_LENGTH * (FRAME_LENGTH/step_length)
+    
     num_frames = int(np.floor(num_frames)) #ALWAYS ROUND DOWN TO REMOVE THE WINDOW WITH NO SIGNAL
     
-    
     all_power = np.array([])
+    
     for i in range(0, num_frames-1):#miss out last digit because the last frame will have little audio anyway
 
         power = mag_and_phase(speech[step_length*i:step_length*i + FRAME_LENGTH])
+        
         all_power = np.append( all_power, power[0:int(FRAME_LENGTH/2)]) # TAKE HALF OF FFT becuase symmetry
         
     return all_power
@@ -123,110 +107,119 @@ def entire_utterance(speech):
 def plot_filter(filter_index, window_index, power_vals, filter_bank, all_windows_filtered):
     #Plot the windowed power, filter and filter result for debugging purposes
     fig, axs = plt.subplots(3)
-    gs = fig.add_gridspec(3, hspace=0)
     #axs = gs.subplots(sharex=True)
     axs[0].plot(all_windows_filtered[window_index,filter_index,0:int(FRAME_LENGTH/2)])
-    axs[1].plot(power_vals_shaped[window_index])
+    axs[1].plot(power_vals[window_index])
     axs[2].plot(filter_bank[filter_index])
     axs[0].set_title("Result of filtering")
     axs[1].set_title("Original power spectrum")
     axs[2].set_title("Filter")
     fig.tight_layout()
+
+
+def plot_dft(dft_mfccs):
+    #Plot dft and spectrogram for debugging
+    index = 10
+    #Plot the dft just to check
+    figgy, axes = plt.subplots()
+    axes.plot(dft_mfccs[index])
+    #plot spectrogram of mfcc
+    plt.figure(figsize=(15,5))
+    axes.imshow(dft_mfccs, aspect='auto', origin='lower');
+
     
 def cepstral_lifter(mfcc):
     #'Ceptral lifting' see- https://maxwell.ict.griffith.edu.au/spl/publications/papers/euro99_kkp_fbe.pdf
     #Doesnt work currently
      num_filters = np.shape(mfcc)[1]
      i = np.linspace(0, num_filters)
-     lifter_coeff = 22
+     lifter_coeff = 22 # arbitrary, good according to the source above
      lifter = 1 + (lifter_coeff/2)*np.sin(np.pi*i/lifter_coeff)
      mfcc *= lifter
     
+def save_to_file(file_name, mfcc):
+    #allow pickle!
+    np.save(file_name, mfcc, allow_pickle=True)
 
-#OPEN AND PLAY SOUNDFILE
-sd.play(r, SAMPLE_FREQUENCY)
+def main(file_dir, file_name):
 
-#CALC THE FILTER BANKS
-filter_bank = plot_melscale()
-
-#PLOT ALL THE FILTERS
-for m in filter_bank:
-     plt.plot(m)
-     plt.xlim(0, FRAME_LENGTH/2 +3)
-     plt.ylim(0,1)
-
-""""" PLOT SPECIFIC FILTERBANK IF NEEDED
-#plt.plot(filter_bank[3])
-fig10, axs10 = plt.subplots(2)
-axs10[0].plot(r)
-axs10[1].plot(np.absolute(np.fft.fft(r))[0:1000])
-"""""
-
-all_power_vals = entire_utterance(r)
-
-#Reshape the power array such that each row corresponds to 1 frame of the FFT power values
-power_vals_shaped = np.reshape(all_power_vals, (int((len(r)/FRAME_LENGTH)*(FRAME_LENGTH/STEP_LENGTH))-1, int(FRAME_LENGTH/2)))
-
-print(power_vals_shaped.shape, "POWER_VALS SHAPE")
-
-summed_filtered_array = np.array([])
-power_vals_filtered = np.array([])
-for row in power_vals_shaped:
-    for filter_peak in filter_bank:
+    r, fs2 = sf.read(file_dir, dtype='float32')
+    
+    sample_frequency = fs2 # Let the sound file decide frequency FOR NOW
+    
+    r = r[10000:-100000] # Take window of signal initially to remove long periods of silence   
+    
+    #CALC THE FILTER BANKS. GET THE POWER SPECTRUM OF EACH WINDOW AND MULTIPLY THEM
+    filter_bank = plot_melscale(sample_frequency)
+    
+    all_power_vals = entire_utterance(r)
+    
+    #Reshape the power array such that each row corresponds to 1 frame of the FFT power values
+    power_vals_shaped = np.reshape(all_power_vals, (int((len(r)/FRAME_LENGTH)*(FRAME_LENGTH/STEP_LENGTH))-1, int(FRAME_LENGTH/2)))
+    
+    print(power_vals_shaped.shape, "POWER_VALS SHAPE")
+    
+    summed_filtered_array = np.array([])
+    
+    power_vals_filtered = np.array([])
+    
+    for row in power_vals_shaped:
         
+        for filter_peak in filter_bank:
+            
+            summed_filtered = np.matmul(row, filter_peak)
+            
+            summed_filtered_array = np.append(summed_filtered_array, summed_filtered)
+    
+    mfccs = summed_filtered_array.reshape(int((len(r)/FRAME_LENGTH)*(FRAME_LENGTH/STEP_LENGTH))-1, FILTER_NUMBER)
+    print(mfccs.shape, "MFCC SHAPE")
+    
+    #TAKE LOG
+    logged_mfccs = np.log10(mfccs)
 
-        summed_filtered = np.matmul(row, filter_peak)
-        summed_filtered_array = np.append(summed_filtered_array, summed_filtered)
-        
-        """THIS SECTION WILL PLOT THE FILTERED POWER VALUES - good for debugging
-        #adjusted_power = np.multiply(row, filter_peak)
-        #summed_filtered = np.sum(adjusted_power)
-        #power_vals_filtered = np.append(power_vals_filtered,adjusted_power)
+    #PERFORM DFT ON LOGGED VALUES
+    #Only take 1-13  of the MFCC's as the others represent fast changes in signal and don't contribute to recognition (?)
+    keep_number = 13
+    
+    dft_mfccs = dct(logged_mfccs, axis=1, norm='ortho')[:, 1 : keep_number]
 
-#Reshape into [FRAME, FILTER, BIN]
-all_windows_filtered = np.reshape(power_vals_filtered, (power_vals_shaped.shape[0], FILTER_NUMBER, int(FRAME_LENGTH/2)))
-print(all_windows_filtered.shape, " filter values reshaped")
+    #At this stage apply cepstral lifting if have time
+    
+    #save file to .npy
+    save_to_file(file_name, dft_mfccs)
+    
+    #ALL DEBUGGING GRAPHS BELOW
 
-#PLOT THE POWER SPECTRUM, FILTER AND RESULT OF FILTER
-filter_index = 41
-window_index = 8
-plot_filter(filter_index, window_index, power_vals_shaped, filter_bank, all_windows_filtered)
-"""""
+    #plot_dft(dft_mfccs) #create spectrogram for debugging
 
-mfccs = summed_filtered_array.reshape(int((len(r)/FRAME_LENGTH)*(FRAME_LENGTH/STEP_LENGTH))-1, FILTER_NUMBER)
-print(mfccs.shape, "MFCC SHAPE")
+    #plt.plot(r) #plot soundfile
+    """
+    #PLOT ALL THE FILTERS
+    for m in filter_bank:
+         plt.plot(m)
+         plt.xlim(0, FRAME_LENGTH/2 +3)
+         plt.ylim(0,1)
+    """
+    """
+    #RESHAPE POWER ARRAY INTO [FRAME, FILTER, BIN] just to check everything is correct
+    #all_windows_filtered = np.reshape(summed_filtered_array, (summed_filtered_array.shape[0], FILTER_NUMBER, int(FRAME_LENGTH/2)))
+    print(all_windows_filtered.shape, " filter values reshaped")
+    
+    #PLOT THE POWER SPECTRUM, FILTER AND RESULT OF FILTER
+    filter_index = 41
+    window_index = 8
+    plot_filter(filter_index, window_index, power_vals_shaped, filter_bank, all_windows_filtered)
+    """
 
-#TAKE LOG THEN DFT
-index = 10
-logged_mfccs = np.log10(mfccs)
-figgy1, axes1 = plt.subplots()
-axes1.plot(logged_mfccs[index])
-axes1.set_title("logged filtered values")
-
-#PERFORM DFT ON LOGGED VALUES
-#Only take 1-13  of the MFCC's as the others represent fast changes in signal and don't contribute to SR (?)
-keep_number = 13
-dft_mfccs = dct(logged_mfccs, axis=1, norm='ortho')[:, 1 : keep_number]
-
-#Apply lifting function to reduce noise 
-
-#Plot the dft just to check
-figgy, axes = plt.subplots()
-axes.plot(dft_mfccs[index])
-
-plt.figure(figsize=(15,5))
-plt.imshow(dft_mfccs, aspect='auto', origin='lower');
-
-
-
-
-
+for audio_file in sorted(glob.glob('Recordings/*.wav')):
+    #Remove file directory and .wav for naming purposes
+    audio_file_name = audio_file[11:-4]
+    
+    main(audio_file, audio_file_name)
+    
+    print(audio_file_name, 'done')
 
 
 #TODO vary feature extraction methods:
 #ENERGY, TEMPORAL DERIVATIVES: VELOCITY, ACCELERATION
 #'SPECTRAL LIFTERING' see- https://maxwell.ict.griffith.edu.au/spl/publications/papers/euro99_kkp_fbe.pdf
-
-
-
-
