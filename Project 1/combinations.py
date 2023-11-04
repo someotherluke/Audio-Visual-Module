6a47f8e78eb116e56b87855e24da93e81fd8ed44
someotherluke <146650936+someotherluke@users.noreply.github.com>
2023-11-04 23:37:01 +0000

    Add files via upload

--- Project 1/combinations.py
+++ Project 1/combinations.py
@@ -1,6 +1,6 @@
 import itertools
 
-# define the possible variables
+#Define the possible variables
 possible_learning_rates = [0.01, 0.001, 0.0001, 0.00001]
 possible_batch_sizes = [8, 16, 32, 64]
 possible_filter_types = ["Mel", "Bark"]
@@ -12,7 +12,7 @@ possible_temporal_included = [1,0]
 possible_no_bins = [32,64,128]
 possible_truncate_no_bin = [8,16,24]
 
-# generate a matrix of all possible combinations
+#Generate a matrix of all possible combinations
 combinations = list(
     itertools.product(
         possible_learning_rates,
@@ -28,18 +28,21 @@ combinations = list(
     )
 )
 
-# Iterate through the combinations
-for combination in combinations:
-    learning_rate = combination[0]
-    batch_size = combination[1]
-    filter_type = combination[2]
-    filter_shape = combination[3]
-    overlap = combination[4]
-    frame_sizes = combination[5]
-    energy_included = combination[6]
-    temporal_included = combination[7]
-    no_bins = combination[8]
-    truncate_no_bins = combination[9]
-    
+#Iterate through the combinations
+def combination():
+    for combination in combinations:
+        learning_rate = combination[0]
+        batch_size = combination[1]
+        filter_type = combination[2]
+        filter_shape = combination[3]
+        overlap = combination[4]
+        frame_sizes = combination[5]
+        energy_included = combination[6]
+        temporal_included = combination[7]
+        no_bins = combination[8]
+        truncate_no_bins = combination[9]
+        
+        print(learning_rate, batch_size, filter_type, filter_shape, overlap, frame_sizes, energy_included, temporal_included, no_bins, truncate_no_bins)
 
-    print(learning_rate, batch_size, filter_type, filter_shape, overlap, frame_sizes, energy_included, temporal_included, no_bins, truncate_no_bins)
+if __name__ == '__main__':
+    combination()
